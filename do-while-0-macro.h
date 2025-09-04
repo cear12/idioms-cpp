a8040c310b83f8c5f9a39ae35334cb0c0a7b9dc2
Ole <ole@MacBook-Air.local>
2025-09-04 12:28:15 +0700

    Added do-while--0-macro idiom

---
+++ do-while-0-macro.h
@@ -0,0 +1,6 @@
+#define MACRO(arg1, arg2) do {  \
+  /* declarations, if any */    \
+  statement1;                   \
+  statement2;                   \
+  /* ... */                     \
+  } while(0) /* (no trailing ; ) */
